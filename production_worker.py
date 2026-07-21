bb6e0a09f5fef1112de18276cbdb0df72abe8083
maniczko <iwo.czajka@gmail.com>
2026-07-22 00:03:25 +0200

    Install atomic publication guard in workers

--- production_worker.py
+++ production_worker.py
@@ -6,12 +6,14 @@ import signal
 import app as app_module
 from durable_job_queue import DurableJobDatabase, DurableJobQueue
 from production_api_policy import install_migrated_sqlite_store
+from production_publication_guard import install_production_publication_guard
 from production_runtime import DurableConversionWorker, durable_database_path
 
 
 def main() -> int:
     database = DurableJobDatabase(durable_database_path())
     install_migrated_sqlite_store(app_module, database=database)
+    install_production_publication_guard(app_module)
     queue = DurableJobQueue(database)
     worker = DurableConversionWorker(
         app_module,
